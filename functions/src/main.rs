fed0833d908283d2e75830988db7b34d3d11025f
Siew Kam Onn <kosiew@gmail.com>
2024-09-28 11:58:10 +0800

    loop with labels

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,21 @@
 fn main() {
-    let x = plus_one(5);
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
 
-    println!("The value of x is: {x}");
-}
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
 
-fn plus_one(x: i32) -> i32 {
-    x + 1
+        count += 1;
+    }
+    println!("End count = {count}");
 }
